4d11a50b16f3b7229edbe32240638ca8ff142c9f
atreyabhatta <atreyabhatta1234@gmail.com>
2026-08-08 20:14:42 +0100

    Add Day 3 code - Modules and Pip in Python
    Added a small practice script demonstrating the difference between
    an external module (pandas - needs pip install) and a built-in
    module (hashlib - comes with Python by default). Used to test that
    importing and using both types of modules works correctly.

---
+++ 03-Day3-Modules-and-Pip/main.py
@@ -0,0 +1,9 @@
+import pandas   # This is an example of an external module
+import hashlib   # This is an example of a built-in module
+
+print("Hi")
+
+# Don't worry about how to use these modules just yet!
+pandas.read_csv("one.csv")
+
+m = hashlib.sha256()
